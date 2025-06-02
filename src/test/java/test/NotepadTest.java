d0e350a71573d90639f04c41fb506d607eb6abca
Neelima T <46975556+Neelimathottasseri@users.noreply.github.com>
2025-06-02 14:08:08 +0530

    Update NotepadTest with close notepad.java

--- src/test/java/test/NotepadTest.java
+++ src/test/java/test/NotepadTest.java
@@ -1,11 +1,13 @@
 package test;
+//From Appium's Windows driver, used for WinAppDriver-based automation.
 import io.appium.java_client.windows.WindowsDriver;
 import io.appium.java_client.windows.WindowsElement;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.remote.DesiredCapabilities;
+
+import org.openqa.selenium.Keys;//sending keyboard short cuts ctrl+s
+import org.openqa.selenium.remote.DesiredCapabilities;//setting app capabilities
 import org.testng.Assert;
 import org.testng.annotations.*;
-import java.io.File;
+import java.io.File;//file handling
 import java.net.URL;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
@@ -21,36 +23,44 @@ public class NotepadTest {
 		caps.setCapability("platformName", "Windows");
 		caps.setCapability("deviceName", "WindowsPC");
 
-		driver = new WindowsDriver<>(new URL("http://127.0.0.1:4723"), caps);
+		driver = new WindowsDriver<>(new URL("http://127.0.0.1:4723"), caps);//connect to winappdriver
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	}
 
 	@Test
 	public void testNotepadSaveFile() throws Exception {
-	
 
+		
 		// Type text
 		WindowsElement editor = driver.findElementByClassName("RichEditD2DPT");
 		editor.sendKeys("Hello World");
 
-		// Ctrl+S to save
+		// Ctrl+S to save,triggers save dialog
 		editor.sendKeys(Keys.chord(Keys.CONTROL, "s"));
 		Thread.sleep(1000);
+		//after this save dialog opens
 
-		// Attach to Save dialog
+		// switch to Save dialog
 		DesiredCapabilities saveCaps = new DesiredCapabilities();
 		String windowHandleHex = driver.getWindowHandle().replace("0x", "");
 		saveCaps.setCapability("appTopLevelWindow", windowHandleHex);
+		//starts a new session for save dialog
 		WindowsDriver<WindowsElement> saveDialog = new WindowsDriver<>(new URL("http://127.0.0.1:4723"), saveCaps);
 
 		//String filePath = System.getProperty("user.home") + "\\Desktop\\TestFile.txt";
 		
+		
 		String filePath = "D:\\TestFile.txt";
+		File file = new File(filePath);
+	//	Assert.assertTrue(file.exists(), "File not saved!");
+		if(file.exists()) {
+			file.delete();
+		}
 		WindowsElement fileNameBox = saveDialog.findElementByClassName("Edit");
 		fileNameBox.clear();
 		fileNameBox.sendKeys(filePath);
 		
-
+		
 		WindowsElement saveBtn = saveDialog.findElementByName("Save");
 		saveBtn.click();
 	
@@ -58,10 +68,15 @@ public class NotepadTest {
 		Thread.sleep(1000);
 
 		// Validate file
-		File file = new File(filePath);
-		Assert.assertTrue(file.exists(), "File not saved!");
 	
-
+		Assert.assertTrue(file.exists(), "File not saved!");
+		Thread.sleep(2000);
+		//WindowsElement fil = driver.findElementByName("File");
+		//WindowsElement exit = driver.findElementByName("Exit");
+		editor.sendKeys(Keys.chord(Keys.chord(Keys.ALT, Keys.F4)));
+		
+		//editor.sendKeys(Keys.chord(Keys.ALT,"X"));
+		
 	}
 
 //	@AfterMethod
